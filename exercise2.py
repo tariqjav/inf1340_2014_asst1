8e2c6c121239d323cd8381e53bc680675a175422
hebe889900 <jackyan63@gmail.com>
2014-10-01 17:16:33 -0400

    #Upload the initial work of exercise2

--- exercise2.py
+++ exercise2.py
@@ -32,21 +32,33 @@ def checksum (upc):
 
     # check type of input
     # raise TypeError if not string
+    if type(upc) is not str:
+        print("Type error")
+        raise TypeError("Invalid input as parameter")
 
     # check length of string
     # raise ValueError if not 12
-    a1 = checksum(1)+checksum(3)+checksum(5)+checksum(7)+checksum(9)+checksum(11)
-    a2 = a1*3 + checksum(2)+checksum(4)+checksum(6)+checksum(8)+checksum(10)
-    a3 = a2 % 10
-    result = 10 - a3
-    #test
+    if len(upc) is not 12:
+        print("Length error")
+        raise ValueError("Length is 12")
+
     # convert string to array
     # hint: use the list function
-
+    checksum = list(upc)
+    #Put the string with 12 digits into a list. 
+    
     # generate checksum using the first 11 digits provided
+    a1 = checksum(1)+checksum(3)+checksum(5)+checksum(7)+checksum(9)+checksum(11)
+    #1. Adding the odd number digits, and multiple by three.
+    a2 = a1*3 + checksum(2)+checksum(4)+checksum(6)+checksum(8)+checksum(10)
+    #2. Adding the even number digits.
+    a3 = a2 % 10
+    #3. Calcultating the modulo ten.
+    result = 10 - a3
+    #4. Subtrating by ten and generating the result.
     # check against the the twelfth digit
-
+    if result == checksum(12):
+           return True
     # return True if they are equal, False otherwise
 
     return False
-

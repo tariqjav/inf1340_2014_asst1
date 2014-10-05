baff10d852539a77ded9b0a47750087782f43699
javeriatariq <javeria.tariq@mail.utoronto.ca>
2014-10-04 21:09:07 -0400

    Added other tests

--- test_exercise3.py
+++ test_exercise3.py
@@ -10,6 +10,12 @@ def test_result():
     """
     assert decide_result("rock", "paper") == 2
     assert decide_result("scissors", "scissors") == 0
+    assert decide_result("rock", "rock") == 0
     assert decide_result("rock", "scissors") == 1
-    # other tests
+    assert decide_result("paper", "paper") == 0
+    assert decide_result("paper", "rock") == 1
+    assert decide_result("paper", "scissors") == 1
+    assert decide_result("scissors", "rock") == 2
+    assert decide_result("scissors", "paper") == 1
+    
 

d73811ce96ab6e68c1b19eb8a45ffc7ac4a06861
javeriatariq <javeria.tariq@mail.utoronto.ca>
2014-10-06 16:51:14 -0400

    added type errors

--- test_exercise3.py
+++ test_exercise3.py
@@ -12,10 +12,20 @@ def test_result():
     assert decide_result("Scissors", "Scissors") == 0
     assert decide_result("Rock", "Rock") == 0
     assert decide_result("Rock", "Scissors") == 1
-    assert decide_result("Paper", "paper") == 0
-    assert decide_result("paper", "rock") == 1
-    assert decide_result("paper", "scissors") == 1
-    assert decide_result("scissors", "rock") == 2
-    assert decide_result("scissors", "paper") == 1
+    assert decide_result("Paper", "Paper") == 0
+    assert decide_result("Paper", "Rock") == 1
+    assert decide_result("Paper", "Scissors") == 1
+    assert decide_result("Scissors", "Rock") == 2
+    assert decide_result("Scissors", "Paper") == 1
 
 
+def test_input():
+    """
+    Inputs that are the incorrect format and length
+    """
+    with exercise3.raises(TypeError):
+        decide_result(1,3)
+        decide_result("orange", "black")
+        decide_result("Hello", 241)
+        decide_result(444444)
+        decide_result("This_is_wrong")
